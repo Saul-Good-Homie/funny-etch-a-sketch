eb8b984e1abc71c2cd25bc33634ee556cca591d2
Saul Belisle <rbelisle1270@gmail.com>
2020-04-07 11:28:28 -0700

    final product shipped

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const container = document.getElementById('container');
 
-// let size = 25;
-
-createGrid(40);
+createGrid(25);
 
 function createGrid(size){
     for (let i = 0; i < size*size; i++) {
@@ -27,44 +25,3 @@ function hoverGrid(e){
       console.log(e.target.className)
     };
 
-
-//function to change to Black
-// function hoverGrid(e){
-//     style.classList.remove('gridBlock');
-//     style.backgroundColor= 'red';
-
-//     };
-
-  //function to change to random RGM
-//   function hoverRGB(e){
-//     var x = Math.floor(Math.random() * 256);
-//     var y = Math.floor(Math.random() * 256);
-//     var z = Math.floor(Math.random() * 256);
-//     var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    
-//     if (e.target.className === "gridBlock"){
-//         e.target.style.background = bgColor;
-//     }
-//     } 
-
-// const menu = document.getElementById('buttons');
-
-// menu.addEventListener("click", function(e){
-//     resetGrid();
-//     if (e.target.id == "rgb") {
-//     container.removeEventListener("mouseover", hoverGrid);
-//     container.addEventListener("mouseover", hoverRGB);
-//     }
-//     else if (e.target.id == "reset") {
-//         container.removeEventListener("mouseover", hoverRGB);
-//         container.addEventListener("mouseover", hoverGrid);
-//     }
-// })
-
-// reset grid to black and white
-    // function resetGrid(){
-    //     const gridTile = document.getElementsByClassName('gridBlock');
-    //       for (i=0; i < gridTile.length; i++){
-    //           gridTile[i].style.background = "white";
-    //           } 
-    //       }
